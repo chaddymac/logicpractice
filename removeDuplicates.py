9c0a4a1e2c2cb1ea0ded5ae2275f4e15b2959a4c
chaddymac <moodychadiamond@yahoo.com>
2022-01-20 18:53:43 -0500

    clean up code

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -6,13 +6,11 @@
 
 
 def removeDuplicates(nums):
-    set_nums = list(set(nums))
-    holder_nums = nums.copy()
-    for i in holder_nums:
-        if nums != set_nums:
+        holder_nums = nums.copy()   
+        for i in holder_nums:
             if nums.count(i) > 1:
                 nums.remove(i)
-    return len(nums)
+        return len(nums)
 
 list1 = [0,0,1,1,1,2,2,3,3,4]
 print(removeDuplicates(list1))
